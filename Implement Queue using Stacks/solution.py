8ed749c223010e7d03be675ae2f0d10d36c36ddd
Alexrlty <siu.pn@ucu.edu.ua>
2026-04-07 21:42:48 +0300

    Implement queue using stacks with Stack class

--- Implement Queue using Stacks/solution.py
+++ Implement Queue using Stacks/solution.py
@@ -1,30 +1,54 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Stack:
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+    def __init__(self):
+        self.top_node = None
+        self.count = 0
+    def push(self, x):
+        node = Stack.Node(x)
+        node.next = self.top_node
+        self.top_node = node
+        self.count += 1
+    def pop(self):
+        if not self.top_node:
+            return None
+        data = self.top_node.data
+        self.top_node = self.top_node.next
+        self.count -= 1
+        return data
+    def peek(self):
+        return self.top_node.data if self.top_node else None
+    def empty(self):
+        return self.top_node is None
+    def size(self):
+        return self.count
+    def queue_push(self, in_stack, out_stack, x):
+        in_stack.push(x)
+    def queue_pop(self, in_stack, out_stack):
+        if out_stack.empty():
+            while not in_stack.empty():
+                out_stack.push(in_stack.pop())
+        return out_stack.pop()
+    def queue_peek(self, in_stack, out_stack):
+        if out_stack.empty():
+            while not in_stack.empty():
+                out_stack.push(in_stack.pop())
+        return out_stack.peek()
+    def queue_empty(self, in_stack, out_stack):
+        return in_stack.empty() and out_stack.empty()
 
 class MyQueue:
     def __init__(self):
-        self.head = None
-        self.tail = None
-
+        self.s1 = Stack()
+        self.s2 = Stack()
+        self.ops = Stack()
     def push(self, x: int) -> None:
-        node = Node(x)
-        if not self.tail:
-            self.head = self.tail = node
-        else:
-            self.tail.next = node
-            self.tail = node
-
+        self.ops.queue_push(self.s1, self.s2, x)
     def pop(self) -> int:
-        data = self.head.data
-        self.head = self.head.next
-        if not self.head:
-            self.tail = None
-        return data
-
+        return self.ops.queue_pop(self.s1, self.s2)
     def peek(self) -> int:
-        return self.head.data
-
+        return self.ops.queue_peek(self.s1, self.s2)
     def empty(self) -> bool:
-        return self.head is None
+        return self.ops.queue_empty(self.s1, self.s2)
